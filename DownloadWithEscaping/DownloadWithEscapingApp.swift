656f52e3d9f65ce4ac90f5d95e35b7f6ba77d289
SeonJungKim <ksj083895@gmail.com>
2021-06-17 23:15:24 +0900

    Initial Commit

---
+++ DownloadWithEscaping/DownloadWithEscapingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithEscapingApp.swift
+//  DownloadWithEscaping
+//
+//  Created by Seogun Kim on 2021/06/17.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithEscapingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
